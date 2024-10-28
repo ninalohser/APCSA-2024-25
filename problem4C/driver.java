f403172fb1ff6efb4db16c907caba115c9e204be
ninalohser <1292592@learn.vsb.bc.ca>
2024-10-28 13:37:41 -0700

    Add files via upload

---
+++ problem4C/driver.java
@@ -0,0 +1,24 @@
+package problem4C;
+
+
+/**
+ * Write a description of class driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class driver{
+    public static void main(String [] args){
+        Review r = new Review();
+        Scanner input = new Scanner (System.in);
+        int x = 10;
+        while (x>0){
+    
+            System.out.println(r.totalSentiment("SimpleReview.txt"));
+        }
+        
+        
+    }
+
+}
