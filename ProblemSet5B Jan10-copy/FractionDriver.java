623e83b96ab93f9f37c1c3da8ef6310ad8a5865b
ninalohser <1292592@learn.vsb.bc.ca>
2025-01-10 14:58:28 -0800

    Add files via upload

---
+++ ProblemSet5B Jan10-copy/FractionDriver.java
@@ -0,0 +1,23 @@
+
+/**
+ * January 7th 2025
+ * Nina Lohser
+ */
+import java.util.Scanner;
+public class FractionDriver{
+
+    public static void main (String [] args){
+
+        System.out.println("Let the Fraction Quiz Begin. Answers should be in lowest terms. Good luck!");
+        String response = input.nextLine();
+        
+        String answer = "";
+        
+        if(response != answer){
+            System.out.println("Wrong, the answer was " + answer);
+        }
+        
+        
+
+    }
+}
